1b1c9e69a62e2c05eaa9de008a28ec1970153c62
xolboyev <javohirxolboyev0232@gmail.com>
2025-06-22 21:37:48 +0500

    dummyjson products

--- src/components/wishlistIcon/WishIcon.jsx
+++ src/components/wishlistIcon/WishIcon.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaHeart } from "react-icons/fa";
+import { CiHeart } from "react-icons/ci";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -9,7 +9,7 @@ const WishIcon = () => {
     <div>
       <Link to="/wishlist" className="relative block">
         <div className="relative">
-          <FaHeart className="text-[22px] text-gray-800 hover:text-[#0074e9]" />
+          <CiHeart className="text-[22px] text-gray-800 hover:text-[#0074e9]" />
           {wishlistItems.length > 0 && (
             <span className="absolute -top-1.5 -right-2 bg-[#ff4444] text-white text-[11px] leading-none w-[18px] h-[18px] rounded-full flex items-center justify-center font-semibold">
               {wishlistItems.length}
