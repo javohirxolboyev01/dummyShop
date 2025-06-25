166f3a9b7eb288d15c760f28633c62bfd6eadecd
xolboyev <javohirxolboyev0232@gmail.com>
2025-06-25 23:46:28 +0500

    E-commercee

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,58 +5,61 @@ import { Button } from "antd";
 const Hero = () => {
   return (
     <div
-      className="relative min-h-screen bg-cover bg-center text-white p-10 flex flex-col justify-center items-end max-[1024px]:items-center max-[1024px]:text-center"
+      className="relative min-h-[710px] bg-cover bg-center text-white flex items-center"
       style={{ backgroundImage: `url(${HeroImg})` }}
     >
-      <div
-        className="bg-white/60 py-14 px-10 max-w-[643px] rounded-lg shadow-lg 
-       max-[1024px]:max-w-[500px] 
-       max-[768px]:max-w-[400px] 
-       max-[480px]:max-w-[90%] max-[480px]:px-6 max-[480px]:py-8"
-      >
-        <p className="text-[#333333] font-Ps mb-2 text-base max-[768px]:text-sm">
-          New Arrival
-        </p>
-
-        <p
-          className="text-[#B88E2F] mb-[17px] font-Pb leading-[65px] text-[52px] 
-       max-[1024px]:text-[42px] 
-       max-[768px]:text-[32px] 
-       max-[480px]:text-[24px] max-[480px]:leading-[38px]"
+      <div className="container mx-auto px-4">
+        <div
+          className="ml-auto 
+  bg-white/60  py-14 px-10 max-w-[643px] rounded-lg shadow-lg
+  max-[1024px]:mx-auto 
+  max-[1024px]:max-w-[500px]
+  max-[768px]:max-w-[400px]
+  max-[480px]:max-w-[90%] max-[480px]:px-6 max-[480px]:py-8"
         >
-          Discover Our
-          <br />
-          New Collection
-        </p>
+          <p className="text-[#333333] font-Ps mb-2 text-base max-[768px]:text-sm">
+            New Arrival
+          </p>
 
-        <p
-          className="text-[18px] font-Pm text-[#333333] mb-[46px] 
-       max-[1024px]:text-[16px] 
-       max-[768px]:text-[14px] 
-       max-[480px]:text-[13px] max-[480px]:mb-6"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-          tellus, luctus nec ullamcorper mattis.
-        </p>
+          <p
+            className="text-[#B88E2F] mb-[17px] font-Pb leading-[65px] text-[52px]
+            max-[1024px]:text-[42px]
+            max-[768px]:text-[32px]
+            max-[480px]:text-[24px] max-[480px]:leading-[38px]"
+          >
+            Discover Our
+            <br />
+            New Collection
+          </p>
 
-        <Button
-          type="primary"
-          className="transition-all duration-300 hover:!bg-[#946d1e] hover:!border-[#946d1e] 
-                text-[16px] font-semibold
-                w-[300px]
-                max-[1024px]:w-[250px]
-                max-[768px]:w-[200px]
-                max-[480px]:w-full"
-          style={{
-            backgroundColor: "#B88E2F",
-            borderColor: "#B88E2F",
-            color: "white",
-            paddingTop: "16px",
-            paddingBottom: "16px",
-          }}
-        >
-          BUY NOW
-        </Button>
+          <p
+            className="text-[18px] font-Pm text-[#333333] mb-[46px]
+            max-[1024px]:text-[16px]
+            max-[768px]:text-[14px]
+            max-[480px]:text-[13px] max-[480px]:mb-6"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis.
+          </p>
+
+          <Button
+            type="primary"
+            className="transition-all duration-300 hover:!bg-[#946d1e] hover:!border-[#946d1e]
+            text-[16px] font-semibold w-[300px]
+            max-[1024px]:w-[250px]
+            max-[768px]:w-[200px]
+            max-[480px]:w-full"
+            style={{
+              backgroundColor: "#B88E2F",
+              borderColor: "#B88E2F",
+              color: "white",
+              paddingTop: "16px",
+              paddingBottom: "16px",
+            }}
+          >
+            BUY NOW
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/components/ReusableHero/Heros.jsx
+++ src/components/ReusableHero/Heros.jsx
@@ -11,39 +11,36 @@ const Heros = ({
 }) => {
   return (
     <div>
-
-    <div
-      className="relative bg-cover bg-center flex items-center justify-center"
-      style={{
+      <div
+        className="relative bg-cover bg-center flex items-center justify-center"
+        style={{
           backgroundImage: `url(${backgroundImage})`,
           height: `${height}px`,
         }}
-        >
-      <div className="absolute inset-0 bg-black/10" />
+      >
+        <div className="absolute inset-0 bg-black/10" />
 
-      <div className="relative z-10 text-center">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-600 mb-3">
-          {title}
-        </h1>
+        <div className="relative z-10 text-center">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-600 mb-3">
+            {title}
+          </h1>
 
-        <div className="flex justify-center items-center gap-2 text-sm text-gray-600">
-          {breadcrumb.map((crumb, index) => (
+          <div className="flex justify-center items-center gap-2 text-sm text-gray-600">
+            {breadcrumb.map((crumb, index) => (
               <span key={crumb} className="flex items-center gap-2">
-              <NavLink
-                to={linkMap[crumb] || "#"}
-                className="hover:underline font-medium"
+                <NavLink
+                  to={linkMap[crumb] || "#"}
+                  className="hover:underline font-medium"
                 >
-                {crumb}
-              </NavLink>
-              {index < breadcrumb.length - 1 && <IoIosArrowForward />}
-            </span>
-          ))}
+                  {crumb}
+                </NavLink>
+                {index < breadcrumb.length - 1 && <IoIosArrowForward />}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
-   
     </div>
-    
-          </div>
   );
 };
 
