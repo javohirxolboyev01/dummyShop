20d59c4d248f401586c556495cd9bbec014c2646
xolboyev <javohirxolboyev0232@gmail.com>
2025-06-27 09:18:46 +0500

    emart24 shop everything online

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),VitePWA()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
